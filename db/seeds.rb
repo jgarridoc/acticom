00f6fc9f73bba43b3fc1c46115ec68ca9b1560ad
Jose_G <garridojose1986@gmail.com>
2021-12-21 17:17:35 -0300

    session a sesion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+r1 = Role.create({ name: 'Regular', description: 'Can read items' })
